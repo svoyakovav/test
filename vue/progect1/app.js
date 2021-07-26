dd3ffab37b7851bd09bdabd32adfe80568a23562
a.svoyakov <a.svoyakov@gmail.com>
2021-07-26 21:21:55 +0400

    *

--- vue/progect1/app.js
+++ vue/progect1/app.js
@@ -2,9 +2,19 @@ const App ={
     data() {
         return {
             counter : 0,
-            title: 'Счетчик'
+            title: 'Счетчик',
+            myplaceholder: 'введите че нить',
+            inputValue : 'dfdfsf'
         } 
+    },
+    methods:{
+        inputChangeHandler(event){
+            //console.log(event.target.value);
+            this.inputValue = event.target.value;
+        }
     }
+
+    
 }
 const app = Vue.createApp(App)
 app.mount('#app');
